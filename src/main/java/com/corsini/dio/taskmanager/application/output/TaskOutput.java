b273f732813f56fffc842f780079d7cbd6efe2d2
Pedro Corsini <pedrocorsini@outlook.com>
2026-09-14 22:41:12 -0300

    feat: add simple front-end html

--- src/main/java/com/corsini/dio/taskmanager/application/output/TaskOutput.java
+++ src/main/java/com/corsini/dio/taskmanager/application/output/TaskOutput.java
@@ -7,7 +7,7 @@ import com.corsini.dio.taskmanager.domain.Task;
 public record TaskOutput(String id, String title, Optional<String> description, String status) {
 
     public static TaskOutput from(Task task) {
-        return new TaskOutput(task.getId().toString(), 
+        return new TaskOutput(task.getId().id().toString(), 
                 task.getTitle(),
                 task.getDescription(),
                 task.getStatus().name());
